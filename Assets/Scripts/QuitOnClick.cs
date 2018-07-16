99c8b58115056594ae52472c90eb4b4f9a9db7ca
Jing Tan <jtan1993@gmail.com>
2018-07-16 21:50:49 +0100

    set up the scene

---
+++ Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+    //need to be public so it can be addressed by the onclick event
+    public void Quit() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+
+#else
+        Application.Quit();
+
+#endif
+    }
+}
