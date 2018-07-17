799d08512aa0fd05af5c3f997b048310890746d1
Jing Tan <jtan1993@gmail.com>
2018-07-17 09:09:03 +0100

    mouse input

---
+++ Assets/Scripts/MuiscPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuiscPlayer : MonoBehaviour {
+    static MuiscPlayer instance = null; //at the start init scene there will be no defined thing called instance
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            //if any musicplayer instance exists, destroy itself
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this; //claim the instance
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+
+    }
+    // Use this for initialization
+    void Start () {
+        Debug.Log("Music player start " + GetInstanceID());
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+    public float TotatlGameUnits = 16;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //map input to mouse position
+        float mousePosInBlocks = Input.mousePosition.x / Screen.width * TotatlGameUnits;
+        print(mousePosInBlocks);
+        
+        //x at the left hand of the play space, keep y pos
+        Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
+        
+        //instance of the current script component
+        paddlePos.x = mousePosInBlocks;
+        this.transform.position = paddlePos;
+
+	}
+}
