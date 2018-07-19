041b79158242cb24576649e88f3a7fdf775ca356
Jing Tan <jtan1993@gmail.com>
2018-07-19 22:11:48 +0100

    launch on mouse click

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Paddle paddle;
+    private Vector3 paddleToBallVector;
+    private bool hasStarted = false;
+    private Rigidbody2D ballRigidBody;
+
+	void Start () {
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+        ballRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        //only hold the ball to the paddle if the game hasn't started
+        if (!hasStarted) {
+            //lock the ball relative to the paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector; //stay in relative position
+
+            //wait for a mouse press to launch
+            if (Input.GetMouseButton(0))
+            {
+                hasStarted = true;
+                ballRigidBody.velocity = new Vector2(2f, 10f);
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,22 +4,21 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour {
     public float TotatlGameUnits = 16;
-    // Use this for initialization
+
     void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
         //map input to mouse position
         float mousePosInBlocks = Input.mousePosition.x / Screen.width * TotatlGameUnits;
         print(mousePosInBlocks);
         
-        //x at the left hand of the play space, keep y pos
+        //constrain x at the left hand of the play space, keep y pos
         Vector3 paddlePos = new Vector3(0.5f, this.transform.position.y, 0f);
         
         //instance of the current script component
-        paddlePos.x = mousePosInBlocks;
+        paddlePos.x = Mathf.Clamp(mousePosInBlocks, -7.0f, 7.0f);
         this.transform.position = paddlePos;
 
 	}
