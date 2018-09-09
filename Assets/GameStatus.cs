ed9a5e95053af58033140f7a48fe43fd0fff678b
Jing Tan <jtan1993@gmail.com>
2018-09-09 16:00:09 +0100

    game speed

---
+++ Assets/GameStatus.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStatus : MonoBehaviour {
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1.0f;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //speed needs to be known for every single frame
+    void Update () {
+        Time.timeScale = gameSpeed;
+	}
+}
