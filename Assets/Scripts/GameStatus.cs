c165a67df6d7bb207dda8e0ce5223daca965ca2c
Jing Tan <jtan1993@gmail.com>
2018-09-11 08:34:30 +0100

    display score

--- Assets/Scripts/GameStatus.cs
+++ Assets/Scripts/GameStatus.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameStatus : MonoBehaviour {
     [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1.0f;
     [SerializeField] int scorePerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
 
     //state variables
     [SerializeField] int currentScore = 0;
 
     void Start () {
-		
+        scoreText.text = "Score: " + currentScore.ToString();
 	}
 
     //speed needs to be known for every single frame
@@ -20,5 +22,6 @@ public class GameStatus : MonoBehaviour {
 
     public void AddToScore() {
         currentScore += scorePerBlockDestroyed;
+        scoreText.text = "Score: " + currentScore.ToString(); 
     }
 }
